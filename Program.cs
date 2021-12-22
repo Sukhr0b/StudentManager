0e3f60efa0f9fdde0ae1b750872fb90f68764127
Sukhr0b <{77388332}+{Sukhr0b}@users.noreply.github.com>
2021-12-22 21:18:28 +0500

    First change With this one you can:   *  Enter student's name and grade   *  Output ther name and grade

--- Program.cs
+++ Program.cs
@@ -4,17 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StudentManager
+namespace SchoolTracker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" Reminder!!!\n If you are too lazy to enter the " +
+                "hole students in your class, just enter 2 hardworking students" +
+                "and 2 lazy students");
+
+            Console.Write(" How many students in your class?\n > ");
+
+            int studentCount = int.Parse(Console.ReadLine());
+            var studentsName = new string[studentCount];
+            var studentsGrade = new int[studentCount];
+
+            for (int eachStudent = 0; eachStudent < studentCount; eachStudent++)
+            {
+                Console.Write(" Student Name:\n > ");
+                studentsName[eachStudent] = Console.ReadLine();
+
+                Console.Write(" Student Grade:\n > ");
+                studentsGrade[eachStudent] = int.Parse(Console.ReadLine());
+            }
+
+            for (int student = 0; student < studentCount; student++)
+            {
+                Console.WriteLine($" Name: {studentsName[student]},  Grade: {studentsGrade[student]}");
+            }
 
 
 
 
-
+            Console.ReadKey();
         }
     }
 }
